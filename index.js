4e9c682067be5c31c0b29a547b2a00bba4d8950e
Dhanya Lakshmi s s <dhanyalakshmi.s.s.06@gmail.com>
2025-02-13 09:49:51 +0530

    updated

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config();
+const mongoose =require('mongoose');
 
 const app = express();
 const port = 3010;
+
+const MONGO_URI=process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+.then(()=>console.log('Connected to database'))
+.catch((err)=>console.log('error connecting to database:',err));
 
 app.use(express.static('static'));
 
